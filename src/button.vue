e59d97db5d6fcb2d170a90df6df9c98bf137e1f0
wanglei <lei.wang@gongkong.com>
2018-11-08 16:21:45 +0800

    优化 button ~

--- src/button.vue
+++ src/button.vue
@@ -1,13 +1,35 @@
 <template>
-    <button class="gk-button" type="button">确定</button>
+    <button class="gk-button" type="button" :class="{[`icon-${iconPosition}`]: true}">
+        <svg v-if="icon" class="icon" aria-hidden="true">
+            <use :xlink:href=`#icon-${icon}`></use>
+        </svg>
+        <span class="icon-content">
+            <slot></slot>
+        </span>
+    </button>
 </template>
 
 <script>
-    export default {}
+    export default {
+        props: {
+            icon: {},
+            iconPosition: {
+                type: String,
+                default: 'left',
+                validator (value) {
+                    return value === 'left' || value === 'right';
+                }
+            }
+        }
+    }
 </script>
 
 <style lang="scss">
     .gk-button {
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        vertical-align: middle;
         height: var(--button-height);
         padding: 0 12px;
         font-size: var(--font-size);
@@ -17,14 +39,21 @@
         color: var(--color-white);
         background: var(--color);
         cursor: pointer;
-        &:hover {
-            background: var(--hover-color);
+        &:hover { background: var(--hover-color);}
+        &:active { background: var(--active-color);}
+        &:focus { outline: none;}
+        > .icon {
+            order: 1;
+            margin-right: .2em;
         }
-        &:active {
-            background: var(--active-color);
-        }
-        &:focus {
-            outline: none;
+        > .icon-content { order: 2; }
+        &.icon-right {
+            > .icon {
+                order: 2;
+                margin-left: .2em;
+                margin-right: 0;
+            }
+            > .icon-content { order: 1; }
         }
     }
 
