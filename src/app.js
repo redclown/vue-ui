cb0f7367b7d2e6baaea6d94692aaa7bd18a3809f
wanglei <lei.wang@gongkong.com>
2018-11-09 12:22:37 +0800

    add button group ~

--- src/app.js
+++ src/app.js
@@ -1,13 +1,15 @@
 import Vue from 'vue';
-import Button from './button';
 import Icon from './icon';
+import Button from './button';
+import ButtonGroup from './button-group';
 
-Vue.component('gk-button', Button);
 Vue.component('gk-icon', Icon);
+Vue.component('gk-button', Button);
+Vue.component('gk-button-group', ButtonGroup);
 
 new Vue({
     el: '#app',
     data: {
-        loading1: false
+        loading1: true
     }
 });
---
+++ src/button-group.vue
@@ -0,0 +1,29 @@
+<template>
+    <div class="gk-button-group">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    export default {}
+</script>
+
+<style lang="scss">
+    .gk-button-group {
+        display: inline-flex;
+        vertical-align: middle;
+        .gk-button {
+            border-radius: 0;
+            border-right: 1px var(--hover-color) solid;
+            &:first-child {
+                border-top-left-radius: var(--border-radius);
+                border-bottom-left-radius: var(--border-radius);
+            }
+            &:last-child {
+                border-right: none;
+                border-top-right-radius: var(--border-radius);
+                border-bottom-right-radius: var(--border-radius);
+            }
+        }
+    }
+</style>
